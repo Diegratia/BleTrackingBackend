49eefad0b5b0f5cf32658f9fda3b17b868133c75
Diegratia <d.febrian2002@gmail.com>
2025-10-20 10:32:30 +0700

    feat: add visitor type migrations

---
+++ Repositories/Migrations/20251020031322_persontype.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class persontype : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "person_type",
+                table: "trx_visitor",
+                type: "nvarchar(255)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "person_type",
+                table: "trx_visitor");
+        }
+    }
+}
