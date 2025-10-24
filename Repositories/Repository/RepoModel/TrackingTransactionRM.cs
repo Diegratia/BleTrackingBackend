da44b46be604a1686e29e657fffbe8cf32011eaa
Diegratia <d.febrian2002@gmail.com>
2025-10-24 09:06:21 +0700

    awdawd

--- Repositories/Repository/RepoModel/TrackingTransactionRM.cs
+++ Repositories/Repository/RepoModel/TrackingTransactionRM.cs
@@ -3,15 +3,21 @@ using Bogus.DataSets;
 
 namespace Repositories.Repository.RepoModel
 {
-    public class TrackingTransactionRM
+     public class TrackingTransactionRM
     {
         public Guid Id { get; set; }
-        public DateTime? Timestamp { get; set; }
-        public string? ReaderName { get; set; }
-        public string? VisitorName { get; set; }
-        public string? MaskedAreaName { get; set; }
-        public string? AlarmStatus { get; set; }
-        public string? ActionStatus { get; set; }
         public DateTime? TransTime { get; set; }
+        public Guid? ReaderId { get; set; }
+        public string ReaderName { get; set; }
+        public Guid? VisitorId { get; set; }
+        public string VisitorName { get; set; }
+        public Guid? MemberId { get; set; }
+        public string MemberName { get; set; }
+        public Guid? FloorplanMaskedAreaId { get; set; }
+        public string FloorplanMaskedAreaName { get; set; }
+        public float? CoordinateX { get; set; }
+        public float? CoordinateY { get; set; }
+        public string AlarmStatus { get; set; }
+        public long? Battery { get; set; }
     }
 }
